8cb56876495350078c5912252eed95ef59b2f77a
ST1NK3R <dyuawduigy@gmail.com>
2024-12-10 22:29:54 +0300

    design auth/log

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import headWithGlasses from './img/head-with-glasses.png';
+import headWithout from './img/head-without.png';
 
 function LoginPage() {
   return (
@@ -15,6 +17,24 @@ function LoginPage() {
         </label>
         <button type="submit">Войти</button>
       </form>
+      <div className='characters1'>
+      <div className="character character-11">
+              <img
+                src={headWithout}
+                alt="Character with bun"
+                width={250}
+                height={200}
+              />
+            </div>
+            <div className="character character-22">
+              <img
+                src={headWithGlasses}
+                alt="Character with glasses"
+                width={250}
+                height={200}
+              />
+            </div>
+            </div>
     </div>
   );
 }
--- src/RegistrationPage.js
+++ src/RegistrationPage.js
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import headWithGlasses from './img/head-with-glasses.png';
+import headWithout from './img/head-without.png';
+// import PurpleCircle from './img/back-purple-circle.png';
+
 
 function RegistrationPage() {
+    const [role, setRole] = useState('');
+
+const handleSubmit = (event) => {
+        event.preventDefault();
+        if (!role) {
+          alert('Пожалуйста, выберите вашу роль.');
+          return;
+        }
+        alert(`Вы зарегистрированы как: ${role}`);
+        // Здесь можно добавить логику для отправки данных на сервер
+      };
+
+
   return (
     <div className="form-page">
       <h2>Регистрация</h2>
       <form>
         <label>
           Имя:
-          <input type="text" required />
+          <input type="text" id='Firsname' required />
+        </label>
+        <label>
+          Фамилия:
+          <input type="text" id='surname' required />
+        </label>
+       
+        <label>
+          Отчество:
+          <input type="text" id='surname2' required />
         </label>
         <label>
           Электронная почта:
@@ -17,8 +43,62 @@ function RegistrationPage() {
           Пароль:
           <input type="password" required />
         </label>
-        <button type="submit">Зарегистрироваться</button>
+        <fieldset>
+          <legend>Выберите вашу роль:</legend>
+          <div className='iam'>Я Ученик</div>
+          <label>
+            <input
+              type="radio"
+              name="role"
+              value="Ученик"
+              checked={role === 'Ученик'}
+              onChange={(e) => setRole(e.target.value)}
+            />
+          </label>
+          <div className='iam'>Я Преподаватель</div>
+          <label>
+          
+            <input
+              type="radio"
+              name="role"
+              value="Преподаватель"
+              checked={role === 'Преподаватель'}
+              onChange={(e) => setRole(e.target.value)}
+            />
+          </label>
+          <div className='iam'>Я Наниматель</div>
+          <label>
+         
+            <input
+              type="radio"
+              name="role"
+              value="Наниматель"
+              checked={role === 'Наниматель'}
+              onChange={(e) => setRole(e.target.value)}
+            />
+          </label>
+        </fieldset>
+        <button className='button-auth' type="submit">Зарегистрироваться</button>
       </form>
+      
+      <div className='characters1'>
+      <div className="character character-11">
+              <img
+                src={headWithout}
+                alt="Character with bun"
+                width={250}
+                height={200}
+              />
+            </div>
+            <div className="character character-22">
+              <img
+                src={headWithGlasses}
+                alt="Character with glasses"
+                width={250}
+                height={200}
+              />
+            </div>
+            </div>
     </div>
   );
 }
